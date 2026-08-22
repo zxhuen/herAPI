a818c306a912d9a4d1825e7e0b5a728a8334ebc9
zxhuen <zxhuen324@gmail.com>
2026-08-22 15:20:10 +0800

    added supabase client

--- app/services/Photo_Services.py
+++ app/services/Photo_Services.py
@@ -5,11 +5,34 @@ from fastapi import HTTPException
 import logging
 
 from forher.app.schemas.Photos import PhotoCreate
+from app.core.supabase_client import supabase
+from uuid import uuid4
+from app.models.Photos import Photo
 
 logger = logging.getLogger(__name__)
 
 
-def add_person_services(db: Session, name: str, photo: bytes):
-    
+def add_photo_services(db: Session, name: str, photo: bytes):
+    file_name = f"{uuid4().hex}.jpg"
+    file_path = f"photos/{file_name}"
+
+    result = supabase.storage.from_("photos").upload(
+        file_path, photo, {"content-type": "image/jpeg"}
+    )
+
+    if not result:
+        raise HTTPException(status_code=500, detail="Failed to upload photo")
+
+    photo_url = supabase.storage.from_("photos").get_public_url(file_path)
+
+    person = Photo(name=name, photo_url=photo_url)
+
+    db.add(person)
+    db.commit()
+    db.refresh(person)
+
+    return person
 
 
+def load_photos_services():
+    print()
