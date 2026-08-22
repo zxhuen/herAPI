003d7e315dae22752be61ad5d93e7d2f876041e0
zxhuen <zxhuen324@gmail.com>
2026-08-22 15:31:04 +0800

    added load photos

--- app/api/Photo.py
+++ app/api/Photo.py
@@ -9,7 +9,7 @@ from app.services import (
     delete_person_services,
 )
 from app.core.limiter import limiter
-from forher.app.services.Photo_Services import add_photo_services
+from forher.app.services.Photo_Services import add_photo_services, load_photos_services
 
 router = APIRouter(prefix="/Photo", tags=["Photo"])
 
@@ -25,3 +25,9 @@ def add_photo(
     photo_bytes = photo.file.read()
 
     return add_photo_services(db, name, photo_bytes)
+
+
+@router.get("/load-photo", response_model=list[PersonResponse])
+@limiter.limit("10/minute")
+def load_photo(request: Request, db: Session = Depends(get_db)):
+    return load_photos_services(db)
